daccc7681ee83dfd600e32e2abfa54ad15969c4c
Shyamali Dolekar <shyamlidolekar@gmail.com>
2022-02-11 22:49:30 +0530

    React Assignment-2 Submission

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -7,12 +7,28 @@ function App(props) {
  const {employee}=props;
     
 return(
-  <div className="container">
-  {/* Start your React code here */}
+  <div className="container App">
+    <div className='profile-container'>
+      <div className='profile'>
+        <img src = {employee.profileImg} width="100" height="100" alt ="Profile Image"/>
+      </div>
+    </div>
+    <div className='name'>
+      <h1>{employee.name}</h1>
+    </div>
+    <div className='profile-details'>
+      <label>Location</label>
+      <strong>{employee.location}</strong>
+    </div>
+    <div className='profile-details'>
+      <label>Blood Group</label>
+      <strong>{employee.bloodGroup}</strong>
+    </div>
+    <div className='profile-details'>
+      <label>Age</label>
+      <strong>{employee.age}</strong>
+    </div>
 </div>
 )
 }
-
-
-
 export default App;
